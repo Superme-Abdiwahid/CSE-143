a60124291340adf228ed8ece18ded274f6a5538c
Superme-Abdiwahid <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-06-15 00:15:44 -0700

    LetterInventory CSe 143 first assignment

---
+++ LetterInventory.java
@@ -0,0 +1,50 @@
+public class LetterInventory {
+  
+  
+public  LetterInventory(String data) {
+  
+}
+  
+public int get(char letter) {
+  
+  
+}
+  
+ public void set(char letter, int value) {
+   
+   
+ }
+  
+public   int size() {
+  
+}
+    
+    
+public  boolean isEmpty() {
+  
+}
+
+
+    
+ public   String toString() {
+   
+ }
+
+
+ public LetterInventory add(LetterInventory other){
+   
+ }
+    
+  
+ LetterInventory add(LetterInventory other) {
+   
+   
+ }
+
+
+  
+  
+  
+  
+  
+}
